1b33072e9e6e4685a6fc9cd761f078baa1064814
fardis <Fardisnazem@gmail.com>
2025-08-26 19:59:46 +0200

    added new updated version of game

--- src/components/apps/Game.tsx
+++ src/components/apps/Game.tsx
@@ -5,10 +5,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 export default function Game({ }: AppWindowProps) {
     const base = import.meta.env.BASE_URL;
 const { unityProvider } = useUnityContext({
-  loaderUrl:  `${base}Build/WebBuilds.loader.js`,
-  dataUrl:    `${base}Build/WebBuilds.data.br`,
-  frameworkUrl:`${base}Build/WebBuilds.framework.js.br`,
-  codeUrl:    `${base}Build/WebBuilds.wasm.br`,
+  loaderUrl:  `${base}Build/unityWeb.loader.js`,
+  dataUrl:    `${base}Build/unityWeb.data.br`,
+  frameworkUrl:`${base}Build/unityWeb.framework.js.br`,
+  codeUrl:    `${base}Build/unityWeb.wasm.br`,
 });
     return <Unity unityProvider={unityProvider} />;
 }
